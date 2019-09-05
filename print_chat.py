9b1a3a344e8256593a1dbc09d4e3d216e026500b
Igor Ivanov <31894518+IVIGOR13@users.noreply.github.com>
2019-09-05 20:12:34 +0300

    Update print_chat.py

--- print_chat.py
+++ print_chat.py
@@ -5,6 +5,8 @@
 import time
 import os
 from termcolor import colored
+from collections import defaultdict
+from datetime import datetime
 import colorama
 
 colorama.init()
@@ -48,7 +50,7 @@ class print_chat:
 
 
     def get_messages_from(self, sender):
-        out = []
+        out = ()
         for i in self.MESSAGES:
             if i['sender'] == sender:
                 out.append(i)
@@ -59,12 +61,22 @@ class print_chat:
         return self.MESSAGES
 
 
+    def get_message(self, number):
+        if number <= len(self.MESSAGES):
+            return self.MESSAGES[len(self.MESSAGES) - number]
+        else:
+            return defaultdict(lambda: "")
+
+
     def get_senders(self):
-        return self.senders
+        out = ()
+        for key in self.senders.keys():
+            out.append(key)
+        return out
 
 
-    def get_skips(self):
-        return self.skips
+    def get_mark(self, number):
+        return self.MESSAGES[len(self.MESSAGES) - number]['mark']
 
 
     def set_colors(self, colors):
@@ -78,21 +90,32 @@ class print_chat:
             if not found:
                 if len(color) == 1:
                     self.senders.append({
-                            'id': self.id_sender,
                             'sender': color[0],
                             'color': 'grey',
                         })
                 else:
                     self.senders.append({
-                            'id': self.id_sender,
                             'sender': color[0],
                             'color': color[1],
                         })
-                self.id_sender += 1
 
 
     def get_time(self):
-        return time.strftime("%H:%M", time.gmtime())
+        if not self.time_full:
+            return datetime.today().strftime("%H:%M")
+        else:
+            return datetime.today().strftime("%d.%m.%y %H:%M")
+
+
+    def set_header(self, text):
+        self.header = text.split('\n')
+        self._print_header()
+
+
+    def _print_header(self):
+        self._clear_screen()
+        for i in self.header:
+            print(i)
 
 
     # returns the number of lines that must be passed to move the cursor to the specified message
@@ -102,11 +125,11 @@ class print_chat:
 
             # counting the number of lines occupied by a message
             m = self.MESSAGES[(len(self.MESSAGES)-1) - i]
-            l = (len(m['sender']) + len(m['message']) + len(m['mark']) + self.len_frame)
+            l = (len(m['sender']) + len(m['text']) + len(m['mark']) + self.len_frame)
 
             # count the number of lines occupied by a skip
             s = 0
-            for j in self.skips[(len(self.skips)-1) - i]:
+            for j in m['skip']:
                 j = str(j)
                 if isinstance(j, str):
                     for k in j.split('\n'):
@@ -119,7 +142,7 @@ class print_chat:
         return lines
 
 
-    def __print_mess(self, sender, text, time, mark):
+    def _print_mess(self, sender, text, time, skip, mark):
 
         if self.is_time:
             print('[{}] '.format(time), end='')
@@ -127,6 +150,7 @@ class print_chat:
         # color selection for printing sender name
         c0, c1 = 'white', 'grey'
 
+        found = False
         for i in self.senders:
             if i['sender'] == sender:
                 c = i['color']
@@ -135,104 +159,131 @@ class print_chat:
                 else:
                     c0, c1 = 'grey', c
                 break
+                found = True
+        if not found:
+            self.senders.append({
+                    'sender': sender,
+                    'color': 'grey',
+                })
 
         print(colored('[' + sender + ']', c0, ('on_' + c1)) + ': ', end='')
         print('{}{}'.format(text, ''.join(mark)), end='\n')
+        for i in skip:
+            print(i)
 
 
-    def add_mark(self, mark):
-        self.up_on_message(1)
-        m = self.MESSAGES[len(self.MESSAGES)-1]['mark']
-        if not m:
-            self.MESSAGES[len(self.MESSAGES)-1].update({
-                    'mark': [str(mark)]
-                })
-        else:
-            m.append(str(mark))
-            self.MESSAGES[len(self.MESSAGES)-1].update({
-                    'mark': m
-                })
-        self._load(1)
+
+    def add_mark(self, number, mark):
+        if not mark == '' and number > 0 and number <= len(self.MESSAGES):
+            self.up_on_message(number)
+            m = self.MESSAGES[len(self.MESSAGES)-number]['mark']
+            if not m:
+                self.MESSAGES[len(self.MESSAGES)-number].update({
+                        'mark': [str(mark)]
+                    })
+            else:
+                m.append(str(mark))
+                self.MESSAGES[len(self.MESSAGES)-number].update({
+                        'mark': m
+                    })
+            self._load(number)
 
 
     def edit_mark(self, number, mark):
-        n = len(self.MESSAGES) - number
-        self.up_on_message(number)
-        self.MESSAGES[n].update({
-                'mark': [str(mark)]
-            })
-        self._load(number)
+        if number > 0 and number <= len(self.MESSAGES):
+            if mark == '':
+                self.remove_mark(number)
+            else:
+                n = len(self.MESSAGES) - number
+                self.up_on_message(number)
+                self.MESSAGES[n].update({
+                        'mark': [str(mark)]
+                    })
+                self._load(number)
 
 
     def remove_mark(self, number):
-        n = len(self.MESSAGES) - number
-        self.up_on_message(number)
-        self.MESSAGES[n].update({
-                'mark': []
-            })
-        self._load(number)
-
-
-    # adds a pass between messages
-    def add_skip(self, text):
-        lines = ((len(str(text))-1) // os.get_terminal_size().columns) + 1
-
-        if not self.skips:
-            self.skips.append([text])
-        else:
-            if not self.skips[len(self.skips)-1]:
-                self.skips[len(self.skips)-1].append(text)
-            else:
-                self.skips[len(self.skips)-1].append(text)
-        if not self.MESSAGES:
-            print(text)
-        else:
-            print(' ' * 8 + text)
-
-
-    def remove_skip(self, number):
-        if number > 0 and number <= len(self.skips):
-            n = len(self.skips) - number
+        if number > 0 and number <= len(self.MESSAGES):
+            n = len(self.MESSAGES) - number
             self.up_on_message(number)
-            self.skips[n].clear()
-            if number == len(self.skips):
-                self._load(number-1)
+            self.MESSAGES[n].update({
+                    'mark': []
+                })
+            self._load(number)
+
+
+    def has_mark(self, number):
+        n = len(self.MESSAGES) - number
+        if self.MESSAGES[n]['mark'] == []:
+            return False
+        else:
+            return True
+
+
+    def get_mark(self, number):
+        n = len(self.MESSAGES) - number
+        return self.MESSAGES[n]['mark']
+
+
+    def add_skip(self, number, text):
+        if not text == '' and number > 0 and number <= len(self.MESSAGES):
+            self.up_on_message(number)
+            m = self.MESSAGES[len(self.MESSAGES)-number]['skip']
+            if not m:
+                self.MESSAGES[len(self.MESSAGES)-number].update({
+                        'skip': [str(text)]
+                    })
             else:
-                self._load(number)
+                m.append(str(text))
+                self.MESSAGES[len(self.MESSAGES)-number].update({
+                        'skip': m
+                    })
+            self._load(number)
 
 
     def edit_skip(self, number, text):
-        if number > 0 and number <= len(self.skips):
-            n = len(self.skips) - number
-            self.up_on_message(number)
-            self.skips[n] = [text]
-            if number == len(self.skips):
-                self._load(number-1)
+        if number > 0 and number <= len(self.MESSAGES):
+            if text == '':
+                self.remove_skip(number)
             else:
+                self.up_on_message(number)
+                self.MESSAGES[len(self.MESSAGES) - number].update({
+                        'skip': [str(text)]
+                    })
                 self._load(number)
 
 
-    def print_skip(self, number):
-        if number > 0 and number <= len(self.skips):
-            if len(self.skips[number]) != 0:
-                for line in self.skips[number]:
-                    print(' ' * 8 + line)
+    def remove_skip(self, number):
+        if number > 0 and number <= len(self.MESSAGES):
+            self.up_on_message(number)
+            self.MESSAGES[len(self.MESSAGES) - number].update({
+                    'skip': []
+                })
+            self._load(number)
+
+
+    def has_skip(self, number):
+        if self.MESSAGES[len(self.MESSAGES) - number]['skip'] == []:
+            return False
+        else:
+            return True
 
 
     # reprints the specified number of messages
     def reload(self, number):
-        if number > 0 and number <= len(self.MESSAGES):
+        if number > 0 and number < len(self.MESSAGES):
             self.up_on_message(number)
+            self._load(number)
+        elif number == len(self.MESSAGES):
+            self._clear_screen()
+            self._print_header()
             self._load(number)
 
 
     def _load(self, number):
         if number > 0 and number <= len(self.MESSAGES):
-            i = len(self.skips) - number
             for m in self.MESSAGES[len(self.MESSAGES)-number:len(self.MESSAGES)]:
-                self.__print_mess(m['sender'], m['message'], m['time'], m['mark'])
-                self.print_skip(i)
-                i += 1
+                self._print_mess(m['sender'], m['text'], m['time'], m['skip'], m['mark'])
 
 
     def remove(self, number):
@@ -241,67 +292,81 @@ class print_chat:
             self._load(number-1)
 
             self.MESSAGES.pop(len(self.MESSAGES) - number)
-            self.skips.pop(len(self.skips) - number)
 
 
     def edit(self, number, text):
         if number > 0 and number <= len(self.MESSAGES):
-            n = len(self.MESSAGES) - number
-            self.up_on_message(number)
-            self.MESSAGES[n].update({
-                    'message': text
-                })
-            self._load(number)
+            if text == '':
+                self.remove(number)
+            else:
+                n = len(self.MESSAGES) - number
+                self.up_on_message(number)
+                self.MESSAGES[n].update({
+                        'text': text
+                    })
+                self._load(number)
 
 
-    def add_message(self, sender, text, mark=[]):
-
+    def add_message_top(self, sender, text, time='', skip=[], mark=[], prnt=True):
         text = " ".join(str(text).split())
-
         if text != '':
 
-            if not self.skips:
-                self.skips.append([])
+            if time == '':
+                time = self.get_time()
 
-            time = self.get_time()
-            self.MESSAGES.append({
-                    'id': self.id_message,
+            self.MESSAGES.insert(0, {
                     'sender': sender,
-                    'message': text,
+                    'text': text,
                     'time': time,
+                    'skip': skip,
                     'mark': mark,
                 })
 
-            self.id_message += 1
+            if prnt:
+                self.up_on_message(self.get_num_messages() - 1)
+                self._print_mess(sender, text, time, skip, mark)
+                self._load(self.get_num_messages()-1)
 
-            self.__print_mess(sender, text, time, mark)
 
-            self.skips.append([])
+    def add_message(self, sender, text, time='', skip=[], mark=[]):
+        text = " ".join(str(text).split())
+        if text != '':
 
-            return self.id_message-1
+            if time == '':
+                time = self.get_time()
+
+            self.MESSAGES.append({
+                    'sender': sender,
+                    'text': text,
+                    'time': time,
+                    'skip': skip,
+                    'mark': mark,
+                })
+            self._print_mess(sender, text, time, skip, mark)
 
 
     def close(self, clr=False):
         self.MESSAGES.clear()
         self.senders.clear()
-        self.skips.clear()
         print('\x1b[A\r', end='')
         if clr:
             self._clear_screen()
 
 
     def __init__(self, time=False):
-
         self.MESSAGES = []
         self.senders = []
-        self.skips = []
+        self.header = []
 
-        self.id_message = 0
-        self.id_sender = 0
-
-        self.is_time = time
-        if self.is_time:
+        self.is_time = False
+        self.time_full = False
+        if time == 'short':
             self.len_frame = 4 + 8
+            self.is_time = True
+        elif time == 'full':
+            self.len_frame = 4 + 8 + 9
+            self.is_time = True
+            self.time_full = True
         else:
             self.len_frame = 4
 
